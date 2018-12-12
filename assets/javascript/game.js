edec9f31c6e64aa9aa63ee16238fcd5e2818dced
evannhenderson <evann.henderson2@yahoo.com>
2018-12-12 16:32:49 -0600

    fuck this white space

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,14 @@
 
 
 var psyGame = {
-    words : ["aerate", "age", "bake", "batter", "blacken", "boil", "bread", "brown", "butterfly", "barbeque", "beat", "blanch", "bone", "brew", "brush", "baste", "bind", "blend", "braise", "broil", "burn", "can", "char-broil", "chunk", "coddle", "cool", "cured", "carmelize", "chill", "churn", "combine", "core", "cut", "char", "chop", "clarify", "congeal", "cream", "debone", "deglaze", "dice", "drain", "decorate", "descale", "dip", "drizzle", "deep fry", "devil", "dough", "dry", "escallop", "ferment", "flambe", "fold", "fricassee", "fry", "fillet", "flavor", "freeze", "frost", "filter", "flip", "French fry", "froth", "garnsih", "grate", "grill", "gel", "gratin", "grind", "glaze", "grease", "hard boil", "heat", "harden", "hull", "hash", "ice", "infuse", "jell", "juice", "julienne", "knead", "layer", "leaven", "macerate", "measure", "mix", "marinate", "melt", "mold", "mash", "microwave", "oil", "oven fry", "overcook", "pan fry", "peel", "pit", "pour", "preserve", "process", "parboil", "percolate", "poach", "precook", "press", "pulp", "pare", "pickle", "pop", "prepare", "pressure cook", "puree", "quarter", "raw", "render", "roll", "reduce", "rise", "rub", "refrigerate", "roast", "salt", "scallop", "scramble", "separate", "shirr", "sieve", "skewer", "slice", "smoke", "soft boil", "steam", "stir", "stuff", "saute", "scoop", "sear", "serve", "shred", "sift", "skim", "sliver", "snip", "souse", "steep", "stir fry", "sweat", "scald", "score", "season", "shell", "shuck", "simmer", "skin", "slow cook", "soak", "sprinkle", "stew", "strain", "sweeten", "uncured", "unmold", "warm", "whip", "wash", "whisk", "wedge"]
+    words : ["aerate", "age", "bake", "batter", "blacken", "boil", "bread", "brown", "butterfly", "barbeque", "beat", "blanch", "bone", "brew", "brush", "baste", "bind", "blend", "braise", "broil", "burn", "can", "char broil", "chunk", "coddle", "cool", "cured", "carmelize", "chill", "churn", "combine", "core", "cut", "char", "chop", "clarify", "congeal", "cream", "debone", "deglaze", "dice", "drain", "decorate", "descale", "dip", "drizzle", "deep fry", "devil", "dough", "dry", "escallop", "ferment", "flambe", "fold", "fricassee", "fry", "fillet", "flavor", "freeze", "frost", "filter", "flip", "french fry", "froth", "garnsih", "grate", "grill", "gel", "gratin", "grind", "glaze", "grease", "hard boil", "heat", "harden", "hull", "hash", "ice", "infuse", "jell", "juice", "julienne", "knead", "layer", "leaven", "macerate", "measure", "mix", "marinate", "melt", "mold", "mash", "microwave", "oil", "oven fry", "overcook", "pan fry", "peel", "pit", "pour", "preserve", "process", "parboil", "percolate", "poach", "precook", "press", "pulp", "pare", "pickle", "pop", "prepare", "pressure cook", "puree", "quarter", "raw", "render", "roll", "reduce", "rise", "rub", "refrigerate", "roast", "salt", "scallop", "scramble", "separate", "shirr", "sieve", "skewer", "slice", "smoke", "soft boil", "steam", "stir", "stuff", "saute", "scoop", "sear", "serve", "shred", "sift", "skim", "sliver", "snip", "souse", "steep", "stir fry", "sweat", "scald", "score", "season", "shell", "shuck", "simmer", "skin", "slow cook", "soak", "sprinkle", "stew", "strain", "sweeten", "uncured", "unmold", "warm", "whip", "wash", "whisk", "wedge"]
 
-
+    
 }
-var currentWord, guesses = [], totalNumberOfGuesses = 0, correctGuesses = [], currentWordArray;
-var winsCounter = 0;
+var currentWord, guesses = [], totalNumberOfGuesses = 0, correctGuesses = [], currentWordArray, availableLetters, winsCounter = 0;
+
+var indexOfSpace;
+
 //outlets
 var guessesRemainingNode = document.querySelector(".guessesRemaining");
 var guessesNode = document.querySelector(".alreadyGuessed");
@@ -16,7 +18,6 @@ var winsNode = document.querySelector(".wins");
 
 
 function restart() {   
-    console.log("restart");
     // select a random word from list of words
     currentWord = psyGame.words[Math.floor(Math.random() * psyGame.words.length)];
     console.warn(currentWord);
@@ -26,10 +27,21 @@ function restart() {
     //splits current word into an array
     currentWordArray = currentWord.split("");
 
+    var lettersExpression = /[a-z]/
+
+    
+
+    correctGuesses = currentWordArray.map( function (letter) {
+        if (letter === " ") return " ";
+        return "_";
+    } );
+
+
     //correct Guesses
-    correctGuesses = currentWord.split("").map((word) => "_");
+
 
     //displays the current word on the DOM
+    
     correctGuessedNode.innerHTML = correctGuesses.join(" ");
 
     //generate amount of guesses based on word count
@@ -45,14 +57,16 @@ function restart() {
     guessesNode.innerHTML = guesses;
 
     winsNode.innerHTML = winsCounter;
+
+    availableLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 }
 
 restart();
 
 
 
-var testForLetters = /^[a-zA-Z]+$/;
 
+var keyPressed;
 //Add global keyup event  listener to capture guessed letters
 document.addEventListener("keyup", function(event) {
     //check to see if user is out of gueses
@@ -62,46 +76,67 @@ document.addEventListener("keyup", function(event) {
         
     }
 
-    var keyPressed = event.key;
-    var foundIndex
+ 
+    
+
+    keyPressed = event.key.toLowerCase();
+    var foundIndex;
+    
+
 
     //check to see if guessed letter is in current word
 
     currentWordArray.map(function (currentLetter, currentIndex, originalArray) { 
         
+
         if (currentLetter != keyPressed) return 
-                
-        // }
+            
         correctGuesses.splice(currentIndex, 1, keyPressed);
         //displays correct letter guesses to DOM
         correctGuessedNode.innerHTML = correctGuesses.join(" ");
         
     })
-   
+
+    // replaces the available letters array with a character that cannot be entered
+    
 
 
     //add key press to already guessed array
-    if (keyPressed.match(testForLetters)) {
-    guesses = guesses.concat([keyPressed]);
-    
-    //update DOM already guessed with current already guessed array
-    guessesNode.innerHTML = guesses;
-    
-    //reduces the guesses counter
-    guessesRemainingNode.innerHTML = --(totalNumberOfGuesses);
-    }
+    availableLetters.map(function (currentLetter, currentIndex) { 
+        
+        if (currentLetter != keyPressed) return 
 
+        guesses = guesses.concat([keyPressed]);
+                
+        //update DOM already guessed with current already guessed array
+        guessesNode.innerHTML = guesses;
+        
+        //reduces the guesses counter
+        guessesRemainingNode.innerHTML = --(totalNumberOfGuesses);
+
+        //replace found keypress with $#%@&^
+        availableLetters[currentIndex] = "$#%@&*^";
+    })
+    console.log(winsCounter, totalNumberOfGuesses, guesses);
+    
     if (totalNumberOfGuesses === 0) {
         alert("You lose!")
+        restart();
     }
 
     if (correctGuesses.join("") === currentWord) {
-        winsCounter++;
-        alert("You win!");
+        correctGuessedNode.innerHTML = currentWord;
+        ++winsCounter;
+        alert(`You guessed the word correctly: ${currentWord}`);
+        restart ();
+        
     }
     
     winsNode.innerHTML = winsCounter;
+
 })
+
+
 
 
 
